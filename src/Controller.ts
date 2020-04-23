77755976164b5b924589ee981dea4eb19ec18991
Oleg <oleg.pav.m@gmail.com>
2020-04-23 21:59:29 +0300

    Fix last stock error message explain.

--- src/Controller.ts
+++ src/Controller.ts
@@ -48,7 +48,7 @@ export class Controller {
         for (const [id, worker] of this.workers) {
             messageLines.push(`Id: ${id}`);
             messageLines.push(`Status:\n${await worker.status()}`);
-            messageLines.push(`Last stock error: ${worker.lastStockError}`);
+            messageLines.push(`Last stock error: ${worker.lastStockError || 'None'}`);
             messageLines.push('\n');
         }
 
